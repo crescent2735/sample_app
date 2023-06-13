196bd0709ed9b1c3e74cc52be9c651ef5a5dbe49
WatanabeMizuki <crescent2735@gmail.com>
2023-06-13 07:16:04 +0000

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -47,7 +47,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
-      delete user_path(@user)
+      delete user_path(@other_user)
     end
     assert_response :see_other
     assert_redirected_to login_url
